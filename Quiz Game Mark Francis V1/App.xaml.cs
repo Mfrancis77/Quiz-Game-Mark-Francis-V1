db64eef4e75cbe2fb9d82d662589a807c07168a2
mdfra <mdfra@MARKSLAPTOP>
2025-09-20 21:52:53 -0400

    Add project files.

---
+++ Quiz Game Mark Francis V1/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Quiz_Game_Mark_Francis_V1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Quiz Game Mark Francis V1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Quiz_Game_Mark_Francis_V1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
